9410d05460d1e3d30aef877671a1cda826119ee6
qarlosalberto <carlosruiznaranjo@gmail.com>
2019-09-10 00:41:30 +0200

    up.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-// exports.Editor        = require('./editor/factory')
+exports.VhdlEditor    = require('./editor/vhdleditor')
+// exports.VerilogEditor = require('./editor/verilogeditor')
 exports.Documenter = require('./documenter/documenter');
-exports.Parser     = require('./parser/vhdlparser');
+exports.VhdlParser     = require('./parser/vhdlparser');
+exports.VerilogParser     = require('./parser/verilogparser');
 // exports.Linter    = require('./linter/linter');
 // exports.StateMachineVhdl = require('./documentation/stateMachineVhdl');
 // exports.StateMachineVerilog = require('./documentation/stateMachineVerilog');
