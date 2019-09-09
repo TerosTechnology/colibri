e6792b40f621d6df60160ff2ab3c9999df308a29
qarlosalberto <carlosruiznaranjo@gmail.com>
2019-09-09 20:31:19 +0200

    corrección.

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// exports.Editor        = require('./editor/editor')
+exports.Documenter = require('./documenter/documenter');
+// exports.Parser    = require('./parser/parser');
+// exports.Linter    = require('./linter/linter');
+// exports.StateMachineVhdl = require('./documentation/stateMachineVhdl');
+// exports.StateMachineVerilog = require('./documentation/stateMachineVerilog');
