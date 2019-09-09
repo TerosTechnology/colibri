8c7310a063098cb911cf33921b2e22feba44b1cb
qarlosalberto <carlosruiznaranjo@gmail.com>
2019-09-09 21:31:05 +0200

    fix bug.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// exports.Editor        = require('./editor/editor')
+// exports.Editor        = require('./editor/factory')
 exports.Documenter = require('./documenter/documenter');
-// exports.Parser    = require('./parser/parser');
+exports.Parser     = require('./parser/vhdlparser');
 // exports.Linter    = require('./linter/linter');
 // exports.StateMachineVhdl = require('./documentation/stateMachineVhdl');
 // exports.StateMachineVerilog = require('./documentation/stateMachineVerilog');
