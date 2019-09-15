d7faa1ed819bb927ab77e9535bb52dcc92a77452
ismael_PR <ismael@ismael>
2019-09-15 19:18:57 +0200

    up

--- test/editor/cocotest_test.py
+++ test/editor/cocotest_test.py
@@ -5,7 +5,7 @@ import os
 def test_adder_vhdl():
     run(vhdl_sources=["../examples/vhdl/example_1.vhd"],
         simulation_args=["--vcd=func.vcd"],
-        toplevel="example_1",
+        toplevel="arith_counter_gray",
         module="cocotb",
         toplevel_lang="vhdl"
     )
