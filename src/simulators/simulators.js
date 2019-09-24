203ca60bab0e702d560f6394a8e2ad19c143e2d1
qarlosalberto <carlosruiznaranjo@gmail.com>
2019-09-24 23:43:51 +0200

    edam

--- src/simulators/simulators.js
+++ src/simulators/simulators.js
@@ -20,19 +20,23 @@ class Manager {
   }
 
   async runCocotb(project){
-    var example = {
-        "name": project["name"],
-        "top_level": project["top_level"],
-        "simulator": 2,
-        "language": 2,
-        "module": project["module"],
-        "files": project["files"]
-    }
-
-    const url = this.url+'/coco/';
+    const url = this.url+'/tests/';
     const getData = async url => {
       try {
-        const response = await axios.post(url,example);
+        const response = await axios.post(url,project);
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    return await getData(url);
+  }
+
+  async runEdalize(project){
+    const url = this.url+'/tests/';
+    const getData = async url => {
+      try {
+        const response = await axios.post(url,project);
         return response.data;
       } catch (error) {
         console.log(error);
