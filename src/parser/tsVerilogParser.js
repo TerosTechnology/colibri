02c073f7a8037348123c709d3ab3d311084e73bc
ismael_PR <ismael_PR@ismael>
2020-04-05 22:50:43 +0200

    fix var

--- src/parser/tsVerilogParser.js
+++ src/parser/tsVerilogParser.js
@@ -22,9 +22,11 @@
 const fs = require('fs');
 const os = require("os");
 const path = require('path');
+var Parser = []
+var VerilogParser = []
 if (process.platform !== "win32") {
-  const Parser = require('tree-sitter');
-  const VerilogParser = require('tree-sitter-verilog');
+  Parser = require('tree-sitter');
+  VerilogParser = require('tree-sitter-verilog');
 }
 
 class tsVerilogParser  {
--- test/parser.js
+++ test/parser.js
@@ -29,22 +29,6 @@ const Parser = Colibri.Parser;
 
 var test_result = true;
 
-for (let x=0;x<6;++x){
-  var ParserLang = [General.LANGUAGES.VHDL];
-  let parser = new Parser.ParserFactory;
-  parser = parser.getParser(ParserLang,"!");
-  let example_vhd = fs.readFileSync('./examples/vhdl/example_'+x+'.vhd' ,'utf8');
-  let example_result  = parser.getAll(example_vhd);
-  let example_exp_result = fs.readFileSync('./examples/vhdl/example_'+x+'.json','utf8');
-  example_exp_result     = JSON.parse(example_exp_result);
-  // console.log(example_result);
-  let rs = compareVhdl(example_result,example_exp_result,"example_"+x+".vhd");
-  console.log("Test " + rs + " ["+"example_"+x+".vhd"+"]");
-  if (rs!= true) {
-    test_result= false;
-  }
-}
-//////////////////////////////////////////////////////////////////////////////
 for (let x=0;x<8;++x){
   var ParserLang = [General.LANGUAGES.VERILOG];
   let parser = new Parser.ParserFactory;
@@ -56,6 +40,22 @@ for (let x=0;x<8;++x){
   // console.log(example_result);
   let rs = compareVerilogTs(example_result,example_exp_result,"example_"+x+".v");
   console.log("Test " + rs + " ["+"example_"+x+".v"+"]");
+  if (rs!= true) {
+    test_result= false;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////
+for (let x=0;x<6;++x){
+  var ParserLang = [General.LANGUAGES.VHDL];
+  let parser = new Parser.ParserFactory;
+  parser = parser.getParser(ParserLang,"!");
+  let example_vhd = fs.readFileSync('./examples/vhdl/example_'+x+'.vhd' ,'utf8');
+  let example_result  = parser.getAll(example_vhd);
+  let example_exp_result = fs.readFileSync('./examples/vhdl/example_'+x+'.json','utf8');
+  example_exp_result     = JSON.parse(example_exp_result);
+  // console.log(example_result);
+  let rs = compareVhdl(example_result,example_exp_result,"example_"+x+".vhd");
+  console.log("Test " + rs + " ["+"example_"+x+".vhd"+"]");
   if (rs!= true) {
     test_result= false;
   }
