d1943ced00219f3ac8b91afd9fcdffc214300b04
ismael_PR <ismael@ismael>
2020-05-11 00:24:06 +0200

    up

--- src/templates/vunit.js
+++ src/templates/vunit.js
@@ -27,15 +27,15 @@ class runpy {
   }
   generate(){
     this.header();
-    this.pythonLibraries()
-    this.setLang()
+    this.python_libraries()
+    this.set_lang()
     this.separator()
-    this.checkSimulator()
-    this.checkCov()
+    this.checks()
+    this.check_simulator()
     this.separator()
-    this.vunitInstance()
-    this.separator()
-    this.addSrc()
+    this.vunit_instance()
+    // this.separator()
+    this.add_src()
     this.addTb()
     this.separator()
     this.flags()
@@ -47,10 +47,10 @@ class runpy {
   header(){
     this.str_out = "# -*- coding: utf-8 -*-\n"
   }
-  pythonLibraries(){
+  python_libraries(){
     this.str_out += "from os.path import join , dirname, abspath\nimport subprocess\nfrom vunit.sim_if.ghdl import GHDLInterface\nfrom vunit.sim_if.factory import SIMULATOR_FACTORY\n"
   }
-  setLang(){
+  set_lang(){
     if (this.str.lang=="vhdl") {
       this.str_out += "from vunit   import VUnit, VUnitCLI\n"
     }
@@ -61,21 +61,56 @@ class runpy {
   separator(){
     this.str_out += "\n##############################################################################\n"
   }
-  checkSimulator(){
+  checks(){
+    this.str_out += "#pre_config func\n"
+    this.str_out += "def pre_config_func():\n"
+    this.str_out += '    """\n'
+    this.str_out += "    Before test.\n"
+    this.str_out += '    """\n'
+    this.str_out += "#post_check func\n"
+    this.str_out += "def post_check_func():\n"
+    this.str_out += '    """\n'
+    this.str_out += "    After test.\n"
+    this.str_out += '    """\n'
+    this.str_out += "def post_check(output_path):\n"
+    this.str_out += "    check = True\n"
+    this.str_out += "    return check\n"
+    this.str_out += "    return post_check\n"
+  }
+  check_simulator(){
     this.str_out += "\n#Check simulator.\n"
     this.str_out += 'print ("=============================================")\n'
-    this.str_out += 'simulator_class = SIMULATOR_FACTORY.select_simulator()\nsimname = simulator_class.name\nprint (simname)\n'
+    this.str_out += 'simname = SIMULATOR_FACTORY.select_simulator().name\n'
+    if (this.str.simulator_suport.modelsim.enable) {
+      this.check_modelsim()
+    }
+    if (this.str.simulator_suport.ghdl.enable) {
+      this.check_ghdl()
+    }
+    this.str_out += 'print ("Simulator = " + simname)\n'
     this.str_out += 'print ("=============================================")\n'
   }
-  vunitInstance(){
+  check_modelsim(){
+    this.str_out += 'code_coverage = False\n'
+    this.str_out += 'if (simname == "modelsim"):\n'
+    this.str_out += '    f= open("modelsim.do","w+")\n'
+    this.str_out += '    f.write("add wave * \nlog -r /*\nvcd file\nvcd add -r /*\n")\n'
+    this.str_out += '    f.close()\n'
+  }
+  check_ghdl(){
+    this.str_out += 'code_coverage = (simname == "ghdl" and \\\n'
+    this.str_out += '                (GHDLInterface.determine_backend("")=="gcc" or  \\\n'
+    this.str_out += '                GHDLInterface.determine_backend("")=="GCC"))\n'
+  }
+  vunit_instance(){
     this.str_out += '\n#VUnit instance.\n'
     this.str_out += 'ui = VUnit.from_argv()\n'
   }
-  addSrc(){
+  add_src(){
     this.str_out += '\n#Add module sources.\n'
     this.str_out += this.str.config["name"] + '_src_lib = ui.add_library("src_lib")\n'
     for(var x = 0; x < this.str.src.length;x++){
-      let file = this.path.relative(this.str.config["outputPath"],this.str.src[x])
+      let file = this.path.relative(this.str.config["output_path"],this.str.src[x])
       file.replace("\\","\\\\")
       this.str_out += this.str.config["name"] + '_src_lib.add_source_files("' + file + '")\n'
     }
@@ -84,10 +119,13 @@ class runpy {
     this.str_out += '\n#Add tb sources.\n'
     this.str_out += this.str.config["name"] + '_tb_lib = ui.add_library("tb_lib")\n'
     for(var x = 0; x < this.str.tb.length;x++){
-      let file = this.path.relative(this.str.config["outputPath"],this.str.tb[x])
+      let file = this.path.relative(this.str.config["output_path"],this.str.tb[x])
       file.replace("\\","\\\\")
       this.str_out += this.str.config["name"] + '_tb_lib.add_source_files("' + file + '")\n'
     }
+  }
+  check_calls(){
+
   }
   run(){
     this.str_out += '\n#Run tests.\n'
@@ -104,10 +142,6 @@ class runpy {
     this.str_out += '    ' + this.str.config["name"] + '_src_lib.add_compile_option   ("ghdl.flags"     , [ '+synopsys_var+'"-fprofile-arcs","-ftest-coverage"'+ psl_var+'])\n'
     this.str_out += '    ' + this.str.config["name"] + '_tb_lib.add_compile_option    ("ghdl.flags"     , [ '+synopsys_var+'"-fprofile-arcs","-ftest-coverage"'+ psl_var+'])\n'
     this.str_out += '    ui.set_sim_option("ghdl.elab_flags"      , ['+synopsys_var+'"-Wl,-lgcov"'+psl_var+'])\n'
-  }
-  checkCov(){
-    this.str_out += '\n#Check GHDL backend.\n'
-    this.str_out += 'code_coverage=False\ntry:\n    if( GHDLInterface.determine_backend("")=="gcc" or  GHDLInterface.determine_backend("")=="GCC"):\n      code_coverage=True\n    else:\n      code_coverage=False\nexcept:\n    print("")\n'
   }
   coverageOut(){
     this.str_out += '\n#Code coverage.\n'
