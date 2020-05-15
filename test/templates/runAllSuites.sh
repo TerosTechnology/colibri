5b0883188ba74c5a75effc1d5db6ebb9c49de089
ismael_PR <ismael_PR@ismael>
2020-05-16 01:13:23 +0200

    up

--- test/templates/runAllSuites.sh
+++ test/templates/runAllSuites.sh
@@ -1,11 +1,11 @@
 # Vunit
 export VUNIT_SIMULATOR=ghdl
 cd examples/vhdl/runpy
-python run_0.py
-python run_1.py
-python run_2.py
-python run_3.py
-python run_4.py
+python3 run_0.py
+python3 run_1.py
+python3 run_2.py
+python3 run_3.py
+python3 run_4.py
 cd ../../..
 # Verilator
 cd examples/verilog
