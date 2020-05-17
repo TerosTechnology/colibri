5769bc97a1703562f944a3c14b412134fe20b9db
ismael_PR <ismael_PR@ismael>
2020-05-17 13:28:07 +0200

    up

--- test/getDoc.js
+++ test/getDoc.js
@@ -22,37 +22,15 @@
 const ln = require('../src/documenter/documenter');
 const fs = require('fs');
 
-var generics = [
-  {'name' : "generic_0",
-   'type' : 'integer' },
-  {'name' : "generic_1",
-   'type' : 'std_logic' },
-  {'name' : "generic_2",
-   'type' : 'integer' }
-];
-
-var ports = [
-  {'name'      : "port_0",
-   'direction' : 'in',
-   'type'      : 'integer' },
-  {'name'      : "port_1",
-   'direction' : 'in',
-   'type'      : 'std_logic' },
-  {'name'      : "port_2",
-   'direction' : 'out',
-   'type'      : 'std_logic_vector(generic_0 downto 0)' }
-];
-
 var structure = fs.readFileSync('./documenter/examples/vhdl/structure.vhd','utf8');
-// structure     = JSON.parse(structure);
 
 var D = new ln.BaseStructure(structure,"vhdl","!");
 var md = D.getMdDoc();
 // D.getPdfDoc('./');
 var html = D.getHtmlDoc();
+// console.log(md)
+console.log(html)
 var svg = D.getDiagram();
 // fs.writeFileSync("svg.svg",svg)
 // fs.writeFileSync("html.html",html)
-// fs.writeFileSync("md.md",md)
-
-// console.log(md);
+fs.writeFileSync("md.md",md)
