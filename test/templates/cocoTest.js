3330e7d74134428ce564724db8ac61f80af349c6
ismael_PR <ismael_PR@ismael>
2020-05-24 19:55:53 +0200

    up

--- test/templates/cocoTest.js
+++ test/templates/cocoTest.js
@@ -23,36 +23,66 @@ const colors = require('colors');
 const path = require('path');
 const fs = require('fs');
 const Colibri = require('../../src/main');
+const Codes = require('../../src/templates/codes')
 
 let path_example_vhdl = 'examples'+path.sep+'vhdl'+path.sep
 let path_example_verilog = 'examples'+path.sep+'verilog'+path.sep
-var structure = fs.readFileSync(path_example_vhdl+'structure.json','utf8');
-var structure_vhdl     = JSON.parse(structure);
-var structure = fs.readFileSync(path_example_verilog+'structure.json','utf8');
-var structure_v     = JSON.parse(structure);
+var structure_vhdl = fs.readFileSync(path_example_vhdl+'example_1.vhd','utf8');
+// var structure_vhdl     = JSON.parse(structure);
+var structure_v = fs.readFileSync(path_example_verilog+'structure.json','utf8');
+// var structure_v     = JSON.parse(structure);
 
 var cocotbpyVhdl = new Colibri.Templates.Templates();
 var cocotbpyV = new Colibri.Templates.Templates();
 
+let options = {
+    "type": "cocotb",
+    "language": "vhdl"
+  };
+let cocotb_vhdl = ""
 let cocotb_vhdl_expected = fs.readFileSync(path_example_vhdl+'cocotbVhdl.py', 'utf8');
-let cocotb_vhdl = cocotbpyVhdl.getCocotbTemplate(structure_vhdl);
+cocotbpyVhdl.getTemplate(Codes.TYPES.COCOTB,structure_vhdl,options).then(cocotb_vhdl => {
+  console.log('****************************************************************');
+  if(cocotb_vhdl_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_vhdl.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
+    console.log("---> Tested: cocotb vhdl --> ok!".green);
+  }
+  else{
+    console.log("---> Tested: cocotb vhdl --> fail!".red);
+    throw new Error('Test error.');
+  }
+});
+
+let options_verilog = {
+    "type": "cocotb",
+    "language": "verilog"
+  };
+let cocotb_verilog = ""
 let cocotb_verilog_expected = fs.readFileSync(path_example_verilog+'cocotbV.py', 'utf8');
-let cocotb_verilog = cocotbpyV.getCocotbTemplate(structure_v);
+cocotbpyV.getTemplate(Codes.TYPES.COCOTB,structure_vhdl,options_verilog).then(cocotb_verilog => {
+  console.log('****************************************************************');
+  if(cocotb_verilog_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_verilog.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
+    console.log("---> Tested: cocotb verilog --> ok!".green);
+  }
+  else{
+    console.log("---> Tested: cocotb verilog --> fail!".red);
+    throw new Error('Test error.');
+  }
+});
 
-console.log('****************************************************************');
-if(cocotb_vhdl_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_vhdl.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
-  console.log("---> Tested: cocotb vhdl --> ok!".green);
-}
-else{
-  console.log("---> Tested: cocotb vhdl --> fail!".red);
-  throw new Error('Test error.');
-}
-
-console.log('****************************************************************');
-if(cocotb_verilog_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_verilog.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
-  console.log("---> Tested: cocotb verilog --> ok!".green);
-}
-else{
-  console.log("---> Tested: cocotb verilog --> fail!".red);
-  throw new Error('Test error.');
-}
+// console.log('****************************************************************');
+// if(cocotb_vhdl_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_vhdl.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
+//   console.log("---> Tested: cocotb vhdl --> ok!".green);
+// }
+// else{
+//   console.log("---> Tested: cocotb vhdl --> fail!".red);
+//   throw new Error('Test error.');
+// }
+//
+// console.log('****************************************************************');
+// if(cocotb_verilog_expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'') === cocotb_verilog.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g,'')){
+//   console.log("---> Tested: cocotb verilog --> ok!".green);
+// }
+// else{
+//   console.log("---> Tested: cocotb verilog --> fail!".red);
+//   throw new Error('Test error.');
+// }
