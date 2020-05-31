c8aeb8439287486400f552aacd9f1b9b53e15f2f
ismael_PR <34767708+smgl9@users.noreply.github.com>
2020-06-01 01:08:43 +0200

    fix models

--- test/templates/component.js
+++ test/templates/component.js
@@ -26,9 +26,9 @@ const Colibri = require('../../src/main');
 const Codes = require('../../src/templates/codes')
 
 var language = [Colibri.General.LANGUAGES.VHDL,Colibri.General.LANGUAGES.VERILOG];
-var tested = [Colibri.Templates.Codes.TYPESCOMPONENTS.COMPONENT,
-              Colibri.Templates.Codes.TYPESCOMPONENTS.INSTANCE,
-              Colibri.Templates.Codes.TYPESCOMPONENTS.SIGNALS];
+var tested = [Codes.TYPESCOMPONENTS.COMPONENT,
+              Codes.TYPESCOMPONENTS.INSTANCE,
+              Codes.TYPESCOMPONENTS.SIGNALS];
 
 let options = {
     'type' : "normal",
@@ -39,41 +39,48 @@ let options = {
       {'parameter' : "Y"}
     ]
 }
-
 var structure_vhdl = []
 var expected_vhdl = []
 for (let x=0;x<tested.length;++x){
   structure_vhdl[x] = fs.readFileSync('examples'+path.sep+language[0]+path.sep+'example_1.vhd','utf8');
   expected_vhdl[x]  = fs.readFileSync('examples'+path.sep+language[0]+path.sep+tested[x] + '.txt','utf8');
 }
-templates_vhdl = new Colibri.Templates.Templates();
+templates_vhdl = new Colibri.Templates.Templates(Codes.TYPES.COMPONENT,options);
 options['language'] = language[0]; // vhdl
 options['type'] = tested[0]; // component
-templates_vhdl.getTemplate(Codes.TYPES.COMPONENT,structure_vhdl[0],options).then(out =>{ check(expected_vhdl[0],out,tested[0],language[0]) });
+templates_vhdl.getTemplate(structure_vhdl[0],options).then(out =>{ check(expected_vhdl[0],out,tested[0],language[0]) });
 options['type'] = tested[1]; // instance
-templates_vhdl.getTemplate(Codes.TYPES.COMPONENT,structure_vhdl[1],options).then(out =>{ check(expected_vhdl[1],out,tested[1],language[0]) });
+templates_vhdl.getTemplate(structure_vhdl[1],options).then(out =>{ check(expected_vhdl[1],out,tested[1],language[0]) });
 options['type'] = tested[2]; // signals
-templates_vhdl.getTemplate(Codes.TYPES.COMPONENT,structure_vhdl[2],options).then(out =>{ check(expected_vhdl[2],out,tested[2],language[0]) });
+templates_vhdl.getTemplate(structure_vhdl[2],options).then(out =>{ check(expected_vhdl[2],out,tested[2],language[0]) });
 
-
+let options_verilog = {
+  'type' : "normal",
+  'language' : 'verilog',
+  'version' : Colibri.General.VERILOGSTANDARS.VERILOG2001,
+  'parameters' : [
+    {'parameter' : "X"},
+    {'parameter' : "Y"}
+  ]
+}
 var structure_verilog = []
 var expected_verilog = []
 for (let x=0;x<tested.length;++x){
   structure_verilog[x] = fs.readFileSync('examples'+path.sep+language[1]+path.sep+'uart.v','utf8');
   expected_verilog[x]  = fs.readFileSync('examples'+path.sep+language[1]+path.sep+tested[x] + '.txt','utf8');
 }
-templates_verilog = new Colibri.Templates.Templates();
+templates_verilog = new Colibri.Templates.Templates(Codes.TYPES.COMPONENT,options_verilog);
 options['language'] = language[1]; // verilog 
 options['type'] = tested[0]; // component
-templates_verilog.getTemplate(Codes.TYPES.COMPONENT,structure_verilog[0],options).then(out =>{ check(expected_verilog[0],out,tested[0],language[1]) });
+templates_verilog.getTemplate(structure_verilog[0],options).then(out =>{ check(expected_verilog[0],out,tested[0],language[1]) });
 options['type'] = tested[1]; // instance
-templates_verilog.getTemplate(Codes.TYPES.COMPONENT,structure_verilog[1],options).then(out =>{ check(expected_verilog[1],out,tested[1],language[1]) });
+templates_verilog.getTemplate(structure_verilog[1],options).then(out =>{ check(expected_verilog[1],out,tested[1],language[1]) });
 options['type'] = tested[2]; // signals
-templates_verilog.getTemplate(Codes.TYPES.COMPONENT,structure_verilog[2],options).then(out =>{ check(expected_verilog[2],out,tested[2],language[1]) });
+templates_verilog.getTemplate(structure_verilog[2],options).then(out =>{ check(expected_verilog[2],out,tested[2],language[1]) });
 
 function check(expected, out,tested,language) {
   console.log('****************************************************************');
-  if(expected_vhdl[0].replace(/\n/g,'').replace(/ /g,'').replace(/\r/g, '') === out.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g, '')){
+  if(expected.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g, '') === out.replace(/\n/g,'').replace(/ /g,'').replace(/\r/g, '')){
     console.log("Testing... " + tested +" "+language+": Ok!".green);
   }
   else{
