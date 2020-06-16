4ab6e21034369ad569e04ba24ec0b4a9717df534
Carlos Alberto <carlos.ruiz@softwareradiosystems.com>
2020-06-16 18:30:10 +0200

    check python version

--- src/projectManager/dependency_graph.js
+++ src/projectManager/dependency_graph.js
@@ -26,6 +26,36 @@ class Dependency_graph {
     this.dependency_graph_svg = "";
   }
 
+  async _get_python_exec(){
+    let command = "python3 --version";
+    let result_command = await this._exec_command(command);
+    //python3 exec exists
+    if (result_command.stderr === ""){
+      return "python3";
+    }
+    else{
+      //check if python path is python3
+      let stdout_arr = result_command.stdout.replace('Python ','').split('.');
+      let python_version = stdout_arr[0];
+      if (python_version !== '3'){
+        return undefined;
+      }
+      else{
+        return "python";
+      }
+    }
+  }
+
+  async _exec_command(command) {  
+    const exec = require('child_process').exec;
+      return new Promise((resolve) => {
+        exec(command, (error, stdout, stderr) => {
+          let result = {'error': error, 'stdout':stdout,'stderr':stderr};
+          resolve(result);
+      });
+    });
+  }
+
   async create_dependency_graph(sources) {
     let str = "";
     if (sources.length !== 0){
@@ -34,10 +64,14 @@ class Dependency_graph {
       }
       str += sources[sources.length-1]['name'];
     }
-
+    let python_exec_path = await this._get_python_exec();
     let py_path = __dirname + path_lib.sep + "vunit_dependency.py";
+    if (python_exec_path === undefined){
+      return undefined;
+    }
     return new Promise(function(resolve, reject) {
-      nopy.spawnPython([py_path, str], { interop: "buffer" }).then(({ code, stdout, stderr }) => {
+      nopy.spawnPython([py_path, str], { interop: "buffer", 
+                    execPath: python_exec_path}).then(({ code, stdout, stderr }) => {
         resolve(stdout);
       });
     });
