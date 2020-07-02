8397ec4733793e9361a8262a74a9bf98178941fc
ismael_PR <34767708+smgl9@users.noreply.github.com>
2020-07-02 22:01:00 +0200

    add test

---
+++ test/examples/vhdl_diagram/example_7.vhd
@@ -0,0 +1,23 @@
+
+library	IEEE;
+use			IEEE.std_logic_1164.all;
+use			IEEE.numeric_std.all;
+
+
+entity counter is
+	generic (
+		number : integer														--! Number
+	);
+	port (
+		clk : in	std_logic;
+		rst : in	std_logic;												
+		inc : in	std_logic
+	);
+end entity;
+
+
+architecture rtl of counter is
+
+begin
+
+end architecture;
