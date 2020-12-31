3cbe9648613e32d4ba09779d86958215b11038b9
Ismael_Perez_R <34767708+smgl9@users.noreply.github.com>
2020-12-31 11:52:19 +0100

    Update parser.js

--- test/parser.js
+++ test/parser.js
@@ -25,24 +25,24 @@ const Colibri = require('../src/main');
 const ParserLib = require('../src/parser/factory');
 const General = Colibri.General;
 
-// for (let x = 8; x < 9; ++x) {
-//   var ParserLang = General.LANGUAGES.VERILOG;
-//   let example_exp_result = fs.readFileSync(__dirname + '/examples/verilog/example_' + x + '.json', 'utf8');
-//   example_exp_result = JSON.parse(example_exp_result);
-//   let example_verilog = fs.readFileSync(__dirname + '/examples/verilog/example_' + x + '.v', 'utf8');
-//   get_structure(ParserLang, "!", example_verilog).then(example_result => {
-//     // console.log(example_result);
-//     let rs = compareVerilogTs(example_result, example_exp_result, "example_" + x + ".v");
-//     console.log("Test " + rs + " [" + "example_" + x + ".v" + "]");
-//     if (rs === true) {
-//       console.log("Test...  OK!".green);
-//     }
-//     else {
-//       console.log("Test...  fail!".red);
-//       throw new Error('Test errors'.red);
-//     }
-//   });
-// }
+for (let x = 0; x < 9; ++x) {
+  var ParserLang = General.LANGUAGES.VERILOG;
+  let example_exp_result = fs.readFileSync(__dirname + '/examples/verilog/example_' + x + '.json', 'utf8');
+  example_exp_result = JSON.parse(example_exp_result);
+  let example_verilog = fs.readFileSync(__dirname + '/examples/verilog/example_' + x + '.v', 'utf8');
+  get_structure(ParserLang, "!", example_verilog).then(example_result => {
+    // console.log(example_result);
+    let rs = compareVerilogTs(example_result, example_exp_result, "example_" + x + ".v");
+    console.log("Test " + rs + " [" + "example_" + x + ".v" + "]");
+    if (rs === true) {
+      console.log("Test...  OK!".green);
+    }
+    else {
+      console.log("Test...  fail!".red);
+      throw new Error('Test errors'.red);
+    }
+  });
+}
 //////////////////////////////////////////////////////////////////////////////
 for (let x = 0; x < 7; ++x) {
   var ParserLang = General.LANGUAGES.VHDL;
